13a2aa2aedf1967dd3a94d3293b306fae05feee1
Giancarlo <cooper458.gm@gmail.com>
2026-01-22 16:25:29 +0100

    added const

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ for (let i = 0; i < 10; i++) {
   axios
     .get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then((response) => {
+      const email = response.data.response;
+      const li = document.createElement("li");
+      li.innerText = email;
       console.log(response.data);
       console.log(response);
+      emailListEl.append;
     })
     .catch((error) => {
       console.log("ERROR:", error);
