e28c19ec45e9f5ee72c6e1aa45987bae2d8ed3c1
Giancarlo <cooper458.gm@gmail.com>
2026-01-22 17:00:30 +0100

    fixed function + added innertext when finished to generating

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 const btnGenerate = document.getElementById("Generate-Button");
 const emailListEl = document.getElementById("email-list");
 const resultsEl = document.getElementById("results");
+
 function generateEmails() {
   emailListEl.innerHTML = "";
   resultsEl.innerText = "Caricamento...";
-}
-for (let i = 0; i < 10; i++) {
-  axios
-    .get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((response) => {
-      const email = response.data.response;
-      const li = document.createElement("li");
-      li.classList.add("list-group-item");
-      li.innerText = `${i + 1}) ${email}`;
-      console.log(response.data);
-      console.log(response);
-      emailListEl.append(li);
-    })
-    .catch((error) => {
-      console.log("ERROR:", error);
-    });
-}
 
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        const email = response.data.response;
+        const li = document.createElement("li");
+
+        li.classList.add("list-group-item");
+        li.innerText = `${i + 1}) ${email}`;
+        emailListEl.append(li);
+
+        if (emailListEl.children.length === 10) {
+          resultsEl.innerText = "Finito!";
+        }
+        console.log(response.data);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log("ERROR:", error);
+      });
+  }
+}
 btnGenerate.addEventListener("click", generateEmails);
