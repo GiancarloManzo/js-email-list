227cbb51ef748684bf3792f4ae12800616ec3d73
Giancarlo <cooper458.gm@gmail.com>
2026-01-22 16:20:18 +0100

    added cicle for

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-axios
-  .get("https://flynn.boolean.careers/exercises/api/random/mail")
-  .then((response) => {
-    console.log(response.data);
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log("ERROR:", error);
-  });
+const emailListEl = document.getElementById("email-list");
+for (let i = 0; i < 10; i++) {
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((response) => {
+      console.log(response.data);
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log("ERROR:", error);
+    });
+}
